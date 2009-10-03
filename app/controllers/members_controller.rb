2a226be90d706f8f1f6856cba02dd55616fa08c0
v_korimilli <vkorimilli@gmail.com>
2009-10-02 19:00:46 -0700

    changes by Bhargavi for Member module

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,6 +1,5 @@
 class MembersController < ApplicationController
-
-  
+      
   # GET /members
   # GET /members.xml
   def index
@@ -16,10 +15,10 @@ class MembersController < ApplicationController
   # GET /members/1.xml
   def show
     begin
-      if params["search_by_email"].nil?
+      if params["search_by_emailid"].nil?
         @members = Member.find(:all)
       else
-        @members = Member.find(:all, :conditions => ['email like ?', "%"+params["search_by_email"]+"%"])
+        @members = Member.find(:all, :conditions => ['emailid like ?', "%"+params["search_by_emailid"]+"%"])
       end
     rescue ActiveRecord::RecordNotFound
       logger.error("Attempt to access invalid member")
@@ -53,17 +52,36 @@ class MembersController < ApplicationController
   # POST /members.xml
   def create
     @member = Member.new(params[:member])
-
-    respond_to do |format|
-      if @member.save
-        flash[:notice] = 'Member was successfully created.'
-        format.html { redirect_to(@member) }
-        format.xml  { render :xml => @member, :status => :created, :location => @member }
+    @validatemember = Member.find(:all, :conditions => ["firstname = ? AND lastname = ? AND emailid = ?", @member.firstname, @member.lastname, @member.emailid
+      ])
+    if @validatemember.length == 0
+      @validateemail = Member.find(:all, :conditions => ["emailid = ? ", @member.emailid])
+      if @validateemail.length == 0
+      @member.save
+      flash[:notice] = 'Member was successfully created.'
+      redirect_to :action => "index"
       else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @member.errors, :status => :unprocessable_entity }
+        flash[:notice] = 'Member already Exists.'
+        render :action => "new"
       end
+    else
+      flash[:notice] = 'Member already Exists.'
+      render :action => "new"
+#      @member.save
+#      flash[:notice] = 'Member was successfully created.'
+#      redirect_to :action => "index"
     end
+
+#    respond_to do |format|
+#      if @member.save
+#        flash[:notice] = 'Member was successfully created.'
+#        format.html { redirect_to(@member) }
+#        format.xml  { render :xml => @member, :status => :created, :location => @member }
+#      else
+#        format.html { render :action => "new" }
+#        format.xml  { render :xml => @member.errors, :status => :unprocessable_entity }
+#      end
+#    end
   end
 
   # PUT /members/1
@@ -95,15 +113,118 @@ class MembersController < ApplicationController
     end
   end
 
-   def memberselect
-    begin
-     @member = Member.new(params[:member])
-     @members = Member.find(:all, :conditions => ["firstname = ? OR lastname = ? OR emailid = ?" ,@member.firstname, @member.lastname, @member.emailid])
-     if @members.length == nil
-       redirect_to  :controller => "users", :action => "memberselect"
-     else
-     end
-   end
+#   def memberselect
+#    begin
+#     @member = Member.new(params[:member])
+#     @members = Member.find(:all, :conditions => ["firstname = ? OR lastname = ? OR emailid = ?" ,@member.firstname, @member.lastname, @member.emailid])
+#     if @members.length == nil
+#       redirect_to  :controller => "users", :action => "memberselect"
+#     else
+#     end
+#   end
+#  end
+
+  def course_schedule_index
+     @course_schedules = []  
+    @member_courses = MemberCourse.find(:all, :conditions => ["member_id = ?", params[:member_id]])
+    if @member_courses.length == 0
+    else
+      for member_course in @member_courses
+        puts member_course.course_schedule_id
+        @c_s = CourseSchedule.find(:all, :conditions => ["id = ? ", member_course.course_schedule_id])
+        for cs in @c_s
+      puts cs.displayname
+      @course_schedules.push(cs)
+    end
+        
+      end
+    end
+#    for d in @course_schedules
+#      puts "sdifsdkfklsdfklsdfnkdsnflksdnfksdfklsdfl"
+#      puts d.displayname
+#    end
+#    
   end
   
+
+  def course_schedule_select
+      puts "member id"
+      puts params[:member_id]
+      puts "in course select"
+      puts "dsfdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"
+      @courses = Course.find(:all, :conditions => ["name = ?" , params[:coursename]])
+
+    for course in @courses
+      puts course.displayName
+      puts course.id
+      @course_schedules = CourseSchedule.find(:all, :conditions => ["course_id = ? AND start_date = ? AND end_date = ? ", course.id, params[:startdate], params[:enddate]])
+    end
+  end
+
+  def course_schedule_insert
+
+    puts "memerdfdfsd"
+    puts params[:member_id]
+    puts params[:id]
+
+    @member_course = MemberCourse.new()
+    @member_course.course_schedule_id = params[:id]
+    @member_course.member_id = params[:member_id]
+    @validatecoursescheduleid = MemberCourse.find(:all, :conditions => ["course_schedule_id = ?", @member_course.course_schedule_id] )
+
+    puts @validatecoursescheduleid.length
+    if @validatecoursescheduleid.length == 0
+      puts "in if loop"
+    @member_course.save
+     flash[:notice] = "The selected Schedule was successfully assigned to this member... Search Again If you want to assign more"
+     render :action => "course_schedule_index", :member_id => params[:member_id]
+    else
+     puts "in else loop"
+     flash[:notice] = "The selected Schedule was already assigned to this member....Search again If you still want to assign more"
+     render :action => "course_schedule_search", :member_id => params[:member_id]
+    end
+  end
+
+  def insert_feedback
+     @feedback = MemberGeneralFeedback.new(params[:feedback])
+     @member_id = @feedback.member_id
+
+    if @feedback.save
+      flash[:notice] = "Your feedback was successfully created"
+    else
+      flash[:notice] = "Failed to create Feedback"
+    end
+     @feedbacks = MemberGeneralFeedback.find(:all, :conditions => ["member_id = ?", @member_id])
+   
+    puts "in insert_feedback"
+    puts params[:feedback]
+    puts params[:member_id]
+     
+  end
+
+  def insert_course_interest
+#  puts params[:name]
+
+    @course_interests = MemberCourseInterest.new(params[:membercourseinterest])
+
+    params[:name].each do |a|
+      puts "vakdd"
+      @course_interests.course_id = a
+      puts @course_interests.course_id
+
+      @find_member = MemberCourseInterest.find(:all, :conditions => ["course_id = ?",@course_interests.course_id])
+      if @find_member.length == 0
+        @course_interests.save
+      end
+    end
+    
+
+  end
+
+  def member_course_interest
+
+    @courses = Course.find(:all)
+
+  end
 end
+ 
