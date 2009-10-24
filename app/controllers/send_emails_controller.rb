e355741aa5ed89f5417b51b7f70ae4f47dd2b8fa
v_korimilli <vkorimilli@gmail.com>
2009-10-24 11:33:43 -0700

    send mails initial page checkin

---
+++ app/controllers/send_emails_controller.rb
@@ -0,0 +1,21 @@
+class SendEmailsController < ApplicationController
+  add_crumb("Send Emails") { |instance| instance.send :sendemails_path }
+
+  def searchmembers
+    @members = Member.paginate :page => params[:page], :per_page => 10
+    @courses = Course.find(:all)
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @members }
+    end
+  end
+
+  def composemessage
+    redirect_to :action => "searchmembers"
+  end
+
+  def completionstatus
+  end
+
+end
